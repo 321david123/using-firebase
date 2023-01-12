e981c23b2f72f80f4489c5555cbb6d8c88bb6a82
David Martinez Rodriguez <321davidmr123@gmail.com>
2023-01-12 16:58:45 -0600

    proyect functionality finally inplemented

--- proyect/src/App.js
+++ proyect/src/App.js
@@ -7,19 +7,27 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 const auth = getAuth(firebaseApp);
 
 function App() {
-  const [usuarioGlobal, setUsuarioGlobal] = useState(null)
+  const [usuarioGlobal, setUsuarioGlobal] = useState(null);
 
-  onAuthStateChanged(auth,(usuarioFirebase) => {
-    if(usuarioFirebase){
+  onAuthStateChanged(auth, (usuarioFirebase) => {
+    if (usuarioFirebase) {
       //codigo en caso de que haya sesion iniciada
       setUsuarioGlobal(usuarioFirebase);
-    }else{
+    } else {
       //codigo en caso de que no haya sesion iniciada
       setUsuarioGlobal(null);
     }
-  })
+  });
 
-  return <>{usuarioGlobal ? <Home/> : <Logueo/>}</>
+  return (
+    <>
+      {usuarioGlobal ? (
+        <Home correoUsuario={usuarioGlobal.email} />
+      ) : (
+        <Logueo />
+      )}
+    </>
+  );
 }
 
 export default App;
--- proyect/src/components/Home.js
+++ proyect/src/components/Home.js
@@ -1,20 +1,78 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-import firebaseApp from '../credenciales';
-import {getAuth, signOut} from "firebase/auth";
-import { Button,Container } from 'react-bootstrap';
+import firebaseApp from "../credenciales";
+import { getAuth, signOut } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+import { Button, Container } from "react-bootstrap";
+
+import AgregarTarea from "./AgregarTareas";
+import ListadoTareas from "./ListadoTareas";
 const auth = getAuth(firebaseApp);
+const firestore = getFirestore(firebaseApp);
 
-export const Home = () => {
-  return <Container>
-        <h4>
-            sesion iniciada
-        </h4>
-        <Button onClick={() => {
-            signOut(auth)
-        }}>Cerrar sesion</Button>
+const Home = ({ correoUsuario }) => {
+  const [arrayTareas, setArrayTareas] = useState(null);
+  const fakedata = [
+    { id: 1, descripcion: "tarea falsa 1", url: "https://picsum.photos/420" },
+    { id: 2, descripcion: "tarea falsa 2", url: "https://picsum.photos/420" },
+    { id: 3, descripcion: "tarea falsa 3", url: "https://picsum.photos/420" },
+  ];
 
-  </Container>
-}
+  async function buscarDocumentoOrCrearDocumento(idDocumento) {
+    //crear referencia la documento
+    const docuRef = doc(firestore, `usuarios/${idDocumento}`);
+    //buscar documento
+    const consulta = await getDoc(docuRef);
+    //revisar si existe
+    if (consulta.exists()) {
+      //si si existe
+      const infoDoc = consulta.data();
+      return infoDoc.tareas;
+    } else {
+      //si no existe
+      await setDoc(docuRef, { tareas: [...fakedata] });
+      const consulta = await getDoc(docuRef);
+      const infoDoc = consulta.data();
+
+      return infoDoc.tareas;
+    }
+  }
+  useEffect(() => {
+    async function fetchTareas() {
+      const tareasFetchadas = await buscarDocumentoOrCrearDocumento(
+        correoUsuario
+      );
+      setArrayTareas(tareasFetchadas);
+    }
+    fetchTareas();
+  }, []);
+
+  return (
+    <Container>
+      <h4>sesion iniciada</h4>
+      <Button
+        onClick={() => {
+          signOut(auth);
+        }}
+      >
+        Cerrar sesion
+      </Button>
+      <hr />
+      <AgregarTarea
+        arrayTareas={arrayTareas}
+        setArrayTareas={setArrayTareas}
+        correoUsuario={correoUsuario}
+      />
+      {arrayTareas ? (
+        <ListadoTareas
+          arrayTareas={arrayTareas}
+          setArrayTareas={setArrayTareas}
+          correoUsuario={correoUsuario}
+        />
+      ) : null}
+    </Container>
+  );
+};
 
 export default Home;
